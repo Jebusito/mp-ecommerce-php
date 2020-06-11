58d7b271664d6f43dcd237acd8a2bcaa5a5bd5fb
iramos <contacto@iramos.com.ar>
2020-06-11 02:23:31 +0000

    parseo a json del body

--- notification.php
+++ notification.php
@@ -1,4 +1,8 @@
 <?php
 error_log("hello, this is a test!");
-error_log(var_dump($_GET));
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+// Converts it into a PHP object
+$data = json_decode($json);
+error_log($data);
 ?>
