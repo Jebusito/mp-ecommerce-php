aec487e32001cb83dbb2edaab99e9d1b995fad8d
iramos <contacto@iramos.com.ar>
2020-06-11 02:46:41 +0000

    checks de heroku

--- notification.php
+++ notification.php
@@ -1,8 +1,5 @@
 <?php
 error_log("hello, this is a test!");
-// Takes raw data from the request
-$json = file_get_contents('php://input');
-// Converts it into a PHP object
-$data = json_decode($json);
-error_log($data);
+$_POST = json_decode(file_get_contents('php://input'), true);
+error_log($_POST);
 ?>
